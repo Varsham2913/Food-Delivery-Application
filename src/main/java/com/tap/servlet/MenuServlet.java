24de06819ab884a87708d16a26e8ef3a7cd8cf35
Varsha M <136302743+Varsham2913@users.noreply.github.com>
2025-02-04 23:07:52 +0530

    Create MenuServlet.java

---
+++ src/main/java/com/tap/servlet/MenuServlet.java
@@ -0,0 +1,45 @@
+package com.tap.Servlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.tap.daoimplementation.MenuDAOImpl;
+import com.tap.model.Menu;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/menu")
+public class MenuServlet  extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		System.out.println("Menu servlet called");
+		
+		int rid=Integer.parseInt(req.getParameter("restaurantId"));
+		System.out.println("Restaurant id="+rid);
+		
+		MenuDAOImpl daoImpl	=new MenuDAOImpl();
+		
+		List<Menu> menuList=daoImpl.getAllMenusByRestaurant(rid);
+		req.setAttribute("menus", menuList);
+		
+		RequestDispatcher rd=req.getRequestDispatcher("menu.jsp");
+		rd.forward(req, resp);
+		
+		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		doGet(req,resp);
+	}
+
+}
+
